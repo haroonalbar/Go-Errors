c7169b2f1622f8cb921e68fc0605d22bf5e42ac8
Haroon Albar <haroon.albar@functionary.co>
2024-04-28 13:01:13 +0530

    errors on go

---
+++ main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+type MyError struct {
+	when time.Time
+	what string
+}
+
+// this works like stringer
+func (i *MyError) Error() string {
+	return fmt.Sprintf("at %v: %v\n", i.when, i.what)
+}
+
+// go has a buit in interface error same thing as fmt.Stringer to handle errors
+// here we are returning error interface type so it will look for Error() method that returns string by default and use it's value 
+func check(i int) error {
+	if i < 10 {
+		return &MyError{
+			when: time.Now(),
+			what: "Bro it's less than 10 not gonna work",
+		}
+	} else {
+		return nil
+	}
+}
+
+func main() {
+	if err := check(10); err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println("Pass aai dee")
+	}
+}
